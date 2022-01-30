bcce78a1c7778621c3a33e2ffa202daebad5bf66
Muhammad Osama <osama94@gmail.com>
2022-01-29 17:45:51 -0800

    Update CUDATemplate.cu

--- CUDATemplate/CUDATemplate.cu
+++ CUDATemplate/CUDATemplate.cu
@@ -1,23 +1,49 @@
-#include <assert.h>
-#include <cuda_runtime.h>
-#include <stdio.h>
-#include "gtest/gtest.h"
+#include <thrust/for_each.h>
+#include <thrust/iterator/counting_iterator.h>
+#include <thrust/device_vector.h>
+#include <thrust/host_vector.h>
 
-__global__ void testKernel()
-{
-    printf("\n I am thread %d\n", threadIdx.x);
+enum my_enum_t {
+  X,  /// for each X
+  Y   /// for each Y
+};
+
+template <my_enum_t t,
+          typename vector_t,
+          typename operator_t,
+          // error: due to overloading
+          std::enable_if_t<t == my_enum_t::X, int> = 0>
+void execute(vector_t& v, operator_t op) {
+  auto data = v.data().get();
+  thrust::for_each(             // for each
+      thrust::cuda::par.on(0),  // stream
+      v.begin(),                // begin
+      v.end(),                  // end
+      // Notice how the two lambdas differ (e.g. of how enum could
+      // lead to different paths we may want to take.)
+      [=] __device__(int x) { op(x); }  // lambda
+  );
 }
 
-TEST(Test, simple)
-{
-    testKernel<<<1, 1>>>();
-    auto err = cudaDeviceSynchronize();
-    EXPECT_EQ(err, cudaSuccess);
+template <my_enum_t t,
+          typename vector_t,
+          typename operator_t,
+          // error: due to overloading
+          std::enable_if_t<t == my_enum_t::Y, int> = 0>
+void execute(vector_t& v, operator_t op) {
+  auto data = v.data().get();
+  thrust::for_each(                              // for each
+      thrust::cuda::par.on(0),                   // stream
+      thrust::counting_iterator<int>(0),         // begin
+      thrust::counting_iterator<int>(v.size()),  // end
+      // Notice how the two lambdas differ (e.g. of how enum could
+      // lead to different paths we may want to take.)
+      [=] __device__(int i) { op(data[i] + (i * i)); }  // lambda
+  );
 }
 
-int main(int argc, char** argv)
-{
-    ::testing::InitGoogleTest(&argc, argv);
-
-    return RUN_ALL_TESTS();
+int main(int argc, char** argv) {
+  thrust::device_vector<int> v(10, 1);
+  execute<my_enum_t::X>(v, [] __device__(int i) { printf("%d\n", i); });
+  execute<my_enum_t::Y>(v, [] __device__(int i) { printf("%d\n", i); });
 }
